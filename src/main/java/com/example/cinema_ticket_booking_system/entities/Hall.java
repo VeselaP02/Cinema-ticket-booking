bb7dd1699c9efd2263f39718fd241f6094e99f32
VeselaP02 <vpetkova0204@abv.bg>
2025-11-19 01:19:53 +0200

    Add new entities in our system

---
+++ src/main/java/com/example/cinema_ticket_booking_system/entities/Hall.java
@@ -0,0 +1,47 @@
+package com.example.cinema_ticket_booking_system.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "halls")
+public class Hall {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "total_seats",nullable = false)
+    private int totalSeats;
+
+    public Hall(String name, int totalSeats) {
+        this.name = name;
+        this.totalSeats = totalSeats;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTotalSeats() {
+        return totalSeats;
+    }
+
+    public void setTotalSeats(int totalSeats) {
+        this.totalSeats = totalSeats;
+    }
+}
